cefcbed93ef20916ce362e48e4dae469af470282
rratilal <rachidratilal@gmail.com>
2024-03-13 15:24:52 +0200

    update achievedAt

--- src/controllers/MilestoneController.ts
+++ src/controllers/MilestoneController.ts
@@ -11,7 +11,7 @@ export default {
                     description,
                     age,
                     achieved,
-                    achievedAt,
+                    achievedAt: new Date(achievedAt),
                     childId,
                 }
             })
